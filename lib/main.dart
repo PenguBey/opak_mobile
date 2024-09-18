3ba6d647da54f77f1fcc0e0ec87097a19afa5913
eefee <124432670+676exe@users.noreply.github.com>
2024-09-18 09:37:26 +0300

    TEST

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter App',
+      title: 'ZekeriyaAbiProje',
       home: MyHomePage(),
     );
   }
@@ -21,12 +21,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
       body: GridView.count(
         childAspectRatio: 1.5,
         crossAxisCount: 2,
@@ -44,18 +43,50 @@ class _MyHomePageState extends State<MyHomePage> {
           _buildItem('Anketler', Icons.poll),
           _buildItem('Verimlilik', Icons.assessment),
           _buildItem('Öneri\nŞikayet', Icons.feedback),
-          _buildItem('', Icons.question_mark),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.menu),
-            label: 'Menu',
+      bottomNavigationBar: Stack(
+        children: [
+          BottomNavigationBar(
+            currentIndex: _selectedIndex,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.menu),
+                label: 'Menü',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profil',
+              ),
+            ],
+            selectedItemColor: Colors.blue,
+            unselectedItemColor: Colors.grey,
+            backgroundColor: Colors.white,
+            type: BottomNavigationBarType.fixed,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profil',
+          Positioned(
+            left: MediaQuery.of(context).size.width / 2 - 28, // Merkezlenmiş
+            bottom: 6, // Hafif yukarıda
+            child: Container(
+              width: 56.0,
+              height: 56.0,
+              decoration: BoxDecoration(
+                color: Colors.blue, // Renk ayarı
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(8.0), // Yuvarlak kenarlar
+              ),
+              child: Center(
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+              ),
+            ),
           ),
         ],
       ),
@@ -64,15 +95,23 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget _buildItem(String title, IconData icon) {
     return Card(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(icon, size: 48.0),
-            SizedBox(height: 8.0),
-            Text(title, textAlign: TextAlign.center),
-          ],
-        ),
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(icon, size: 48.0),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(title, textAlign: TextAlign.right),
+            ),
+          ),
+        ],
       ),
     );
   }
