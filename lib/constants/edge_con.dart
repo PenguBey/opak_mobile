4f7d6c9efc47e2b6d4e2000dcd00fa16c6b79c47
PenguBey <omer39ctnr@gmail.com>
2024-09-22 17:03:40 +0300

    maas grafigi
    maas grafigini biraz padding ve renk verdim

--- lib/constants/edge_con.dart
+++ lib/constants/edge_con.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class EdgeCon {
   static const EdgeInsets card = EdgeInsets.all(8.0);
+  static const EdgeInsets chart = EdgeInsets.all(24.0);
   static const EdgeInsets miniBaslik = EdgeInsets.only(bottom: 8.0);
   static const edgeSizedTop = SizedBox(
     height: 40,
