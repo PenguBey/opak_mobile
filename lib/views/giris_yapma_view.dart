767137f62873f042884707d0d5ffe2504b71ab95
eefee <124432670+676exe@users.noreply.github.com>
2024-09-30 15:34:22 +0300

    efe2
    .

--- lib/views/giris_yapma_view.dart
+++ lib/views/giris_yapma_view.dart
@@ -13,36 +13,88 @@ class GirisYapmaView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Giriş Sayfası'),
         centerTitle: true,
+        backgroundColor: const Color.fromARGB(136, 255, 255, 255),
+        elevation: 4,
       ),
-      body: Padding(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 0, 0, 0),
+              Colors.lightBlueAccent
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Kullanıcı Adı',
-                border: OutlineInputBorder(),
-              ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: usernameController,
+                  decoration: InputDecoration(
+                    labelText: 'Kullanıcı Adı',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.8),
+                    prefixIcon:
+                        const Icon(Icons.person, color: Colors.blueAccent),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Şifre',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.8),
+                    prefixIcon:
+                        const Icon(Icons.lock, color: Colors.blueAccent),
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 24),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.offNamed('/main_menu');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(136, 255, 255, 255),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 100, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    elevation: 5,
+                  ),
+                  child: const Text('Giriş Yap',
+                      style: TextStyle(
+                          fontSize: 18, color: Color.fromARGB(179, 0, 0, 0))),
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Şifreyi Unuttum?',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Şifre',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () {
-                Get.offNamed('/main_menu');
-              },
-              child: const Text('Giriş Yap'),
-            ),
-          ],
+          ),
         ),
       ),
     );
